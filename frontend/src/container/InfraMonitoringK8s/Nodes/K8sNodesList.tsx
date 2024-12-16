f7575dd9a00f707ef9204331e6ae768dd2ad427e
amlannandy <amlannandy5@gmail.com>
2024-12-16 10:50:27 +0530

    feat: implement node details page

--- frontend/src/container/InfraMonitoringK8s/Nodes/K8sNodesList.tsx
+++ frontend/src/container/InfraMonitoringK8s/Nodes/K8sNodesList.tsx
@@ -28,6 +28,7 @@ import { GlobalReducer } from 'types/reducer/globalTime';
 import K8sHeader from '../K8sHeader';
 import LoadingContainer from '../LoadingContainer';
 import { dummyColumnConfig } from '../utils';
+import NodeDetails from './NodeDetails';
 import {
 	defaultAddedColumns,
 	formatDataForTable,
@@ -57,7 +58,7 @@ function K8sNodesList({
 		order: 'asc' | 'desc';
 	} | null>(null);
 
-	// const [selectedNodeUID, setselectedNodeUID] = useState<string | null>(null);
+	const [selectedNodeUID, setselectedNodeUID] = useState<string | null>(null);
 
 	const pageSize = 10;
 
@@ -254,10 +255,10 @@ function K8sNodesList({
 		logEvent('Infra Monitoring: K8s list page visited', {});
 	}, []);
 
-	// const selectedNodeData = useMemo(() => {
-	// 	if (!selectedNodeUID) return null;
-	// 	return nodesData.find((node) => node.nodeUID === selectedNodeUID) || null;
-	// }, [selectedNodeUID, nodesData]);
+	const selectedNodeData = useMemo(() => {
+		if (!selectedNodeUID) return null;
+		return nodesData.find((node) => node.nodeUID === selectedNodeUID) || null;
+	}, [selectedNodeUID, nodesData]);
 
 	const handleRowClick = (record: K8sNodesRowData): void => {
 		if (groupBy.length === 0) {
@@ -482,7 +483,11 @@ function K8sNodesList({
 					}}
 				/>
 			)}
-			{/* TODO - Handle Node Details flow */}
+			<NodeDetails
+				node={selectedNodeData}
+				isModalTimeSelection
+				onClose={handleCloseNodeDetail}
+			/>
 		</div>
 	);
 }
