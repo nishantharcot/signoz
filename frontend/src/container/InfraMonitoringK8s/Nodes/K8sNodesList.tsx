c16adc06f21099e33d0cbe9f5eebe4e40bb32436
amlannandy <amlannandy5@gmail.com>
2024-12-19 12:58:02 +0530

    chore: rebase fixes

--- frontend/src/container/InfraMonitoringK8s/Nodes/K8sNodesList.tsx
+++ frontend/src/container/InfraMonitoringK8s/Nodes/K8sNodesList.tsx
@@ -372,9 +372,9 @@ function K8sNodesList({
 		);
 	};
 
-	// const handleCloseNodeDetail = (): void => {
-	// 	setselectedNodeUID(null);
-	// };
+	const handleCloseNodeDetail = (): void => {
+		setselectedNodeUID(null);
+	};
 
 	const showsNodesTable =
 		!isError &&
